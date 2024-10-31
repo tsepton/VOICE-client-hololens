daea471c1edcc4fb05794350f698f84c837157fe
Thibaut Septon <tsepton@gmail.com>
2024-10-31 16:21:10 +0100

    Refactor script

--- Assets/Screenshot.cs
+++ Assets/Screenshot.cs
@@ -20,6 +20,11 @@ public class Screenshot : MonoBehaviour
         });
     }
 
+    void OnDestroy()
+    {
+        OnStoppedPhotoMode();
+    }
+
     public void TakePhoto()
     {
         Debug.Log("Taking a photo.");
@@ -38,33 +43,24 @@ public class Screenshot : MonoBehaviour
 
     private void OnPhotoModeStarted(PhotoCapture.PhotoCaptureResult result)
     {
-        if (result.success)
+        if (!result.success)
         {
-            string filename = string.Format(@"CapturedImage{0}_n.jpg", Time.time);
-            string filePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
+            Debug.LogError("Unable to start photo mode");
+            return;
+        }
 
-            photoCaptureObject.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.JPG, OnCapturedPhotoToDisk);
-        }
-        else
+        // TODO - base64 available property
+        string filename = string.Format(@"CapturedImage{0}_n.jpg", Time.time);
+        string filePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
+
+        photoCaptureObject.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.JPG, delegate (PhotoCapture.PhotoCaptureResult result)
         {
-            Debug.LogError("Unable to start photo mode!");
-        }
+            if (result.success) Debug.Log("Saved Photo to disk");
+            else Debug.Log("Failed to save Photo to disk");
+        });
     }
 
-    void OnCapturedPhotoToDisk(PhotoCapture.PhotoCaptureResult result)
-    {
-        if (result.success)
-        {
-            Debug.Log("Saved Photo to disk!");
-            photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
-        }
-        else
-        {
-            Debug.Log("Failed to save Photo to disk");
-        }
-    }
-
-    void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    void OnStoppedPhotoMode()
     {
         photoCaptureObject.Dispose();
         photoCaptureObject = null;
