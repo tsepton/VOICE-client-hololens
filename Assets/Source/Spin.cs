82c998601ee97376300a86a0299c38a24cc340a9
Thibaut Septon <tsepton@gmail.com>
2025-02-10 17:37:15 +0100

    Fix spin component

--- Assets/Source/Spin.cs
+++ Assets/Source/Spin.cs
@@ -5,20 +5,12 @@ using UnityEngine;
 public class Spin : MonoBehaviour {
 
 	[SerializeField] private RectTransform trans;
-	
-	private Quaternion initialRotation;
 
 	void Start() {
 		trans = gameObject.GetComponent<RectTransform>();
-		initialRotation = trans.rotation;
 	}
 
 	void FixedUpdate() {
-		trans.Rotate(new Vector3(0,0,-180) * Time.deltaTime);
-	}	
-	
-	void OnDisable() 
-	{
-		trans.rotation = initialRotation;
+		trans.Rotate(new Vector3(0, 0, -180) * Time.deltaTime);
 	}
 }
